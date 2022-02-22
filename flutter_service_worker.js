0068d456b5680f30f8f8d1fc0b20b9616fb3b225
Nicha <NichaRoj@gmail.com>
2022-02-22 15:24:42 +0700

    Built web
    Branch: master
    Commit: d3083ec525e7489581de5d89675f18b4ae4517c8
    
    package:peanut 4.1.1

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -6,7 +6,7 @@ const RESOURCES = {
   "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/NOTICES": "5e7feb15b4ea57eab8ff9bc6da0322de",
+"assets/NOTICES": "f82734ad34cefef897ee34abf82dd78f",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "canvaskit/canvaskit.js": "c2b4e5f3d7a3d82aed024e7249a78487",
 "canvaskit/canvaskit.wasm": "4b83d89d9fecbea8ca46f2f760c5a9ba",
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "cdd668e188dfb331140ff51dd4bffa0f",
-"/": "cdd668e188dfb331140ff51dd4bffa0f",
-"main.dart.js": "698d3f73a8ba51ef1f2da45606c94e95",
+"index.html": "668f26f630f5ea48c191f909df53d6d2",
+"/": "668f26f630f5ea48c191f909df53d6d2",
+"main.dart.js": "1bd02201fcb2b4f9caf153bf1a69a280",
 "manifest.json": "1d7f920caadc95f1d79bad984bb5b294",
 "version.json": "2f8ee0fd4fc33e11bf7a3d9d821b007b"
 };
